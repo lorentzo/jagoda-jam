edc3b9e98f03cbb4360bf849d10850833b11203d
Borna Bešić <git.o8yim@aleeas.com>
2023-07-11 21:58:51 +0200

    Hide the exit button for web build in the main menu

--- jagoda/Scenes/MainMenu.gd
+++ jagoda/Scenes/MainMenu.gd
@@ -4,6 +4,10 @@ const GAME_SCENE: String = "res://Scenes/Game.tscn"
 
 @onready var loading = get_node("/root/Loading")
 
+func _ready():
+	if OS.get_name() != "Web":
+		$MainVBox/MenuVBox/ExitButton.show()
+
 func _on_play_button_pressed():
 	self.hide()
 	loading.load_scene(GAME_SCENE)
