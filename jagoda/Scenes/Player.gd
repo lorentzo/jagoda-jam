0e8607f746d261c8699a9894d257954050366a17
Borna Bešić <git.o8yim@aleeas.com>
2023-07-11 23:06:36 +0200

    Add plant refreshing

--- jagoda/Scenes/Player.gd
+++ jagoda/Scenes/Player.gd
@@ -1,3 +1,4 @@
+class_name Player
 extends CharacterBody2D
 
 const WALK_SPEED = 150.0
@@ -7,6 +8,7 @@ signal player_freshness_changed(freshness)
 
 var freshness: float = 100
 var current_sun_intensity = 0
+var visible_plants: Dictionary = {}
 
 func on_sun_intensity_changed(sun_intensity):
 	self.current_sun_intensity = sun_intensity
@@ -24,9 +26,25 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("walk-down"):
 		walk_velocity.y = 1
 
+	if Input.is_action_pressed("use-item"):
+		for plant in self.visible_plants.keys():
+			plant.refresh(5.0 * delta)
+
+	if walk_velocity.x != 0:
+		$RefreshArea.scale.x = walk_velocity.x
+
 	self.velocity = walk_velocity.normalized() * WALK_SPEED
 	move_and_slide()
 	
 	var freshness_lost_per_second = self.current_sun_intensity * MAX_FRESHNESS_LOST_PER_SECOND
 	self.freshness = max(self.freshness - freshness_lost_per_second * delta, 0)
 	self.player_freshness_changed.emit(self.freshness)
+
+func _on_refresh_area_area_entered(area):
+	if area is Plant:
+		area.set_freshness_visible(true)
+		self.visible_plants[area] = true
+	
+func _on_refresh_area_area_exited(area):
+	if self.visible_plants.erase(area):
+		area.set_freshness_visible(false)
