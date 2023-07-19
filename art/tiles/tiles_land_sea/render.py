a64583cb5f681da155dfcfdbbaf7ef9ccd7c3c34
Lovro Bosnar <lovro.bosnar1@gmail.com>
2023-07-19 08:22:25 +0200

    removing blocking tiles from godot, updating tiles

--- art/tiles/tiles_land_sea/render.py
+++ art/tiles/tiles_land_sea/render.py
@@ -11,13 +11,14 @@ collection_name = "landIN_seaOUT"
 for obj in bpy.data.collections[collection_name].all_objects:
     obj.hide_render = False
 
+i = 0
 for obj in bpy.data.collections[collection_name].all_objects:
     # Store standard location of current object.
     standard_location = mathutils.Vector(obj.location)
     # Move current object to camera.
     obj.location = mathutils.Vector((0,0,0))
     # Create render path.
-    render_path = os.path.join(os.getcwd(), obj.name)
+    render_path = os.path.join(os.getcwd(), collection_name + "_" + str(i))
     print(render_path)
     bpy.context.scene.render.filepath = render_path
     # Render.
@@ -25,3 +26,5 @@ for obj in bpy.data.collections[collection_name].all_objects:
     bpy.ops.render.render(write_still = True)
     # Restore object location.
     obj.location = standard_location
+    # Increase counter for naming.
+    i = i + 1
