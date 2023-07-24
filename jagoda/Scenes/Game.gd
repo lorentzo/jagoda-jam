7c5469b3d47ca2de470ca1dbe510a7c8f7cb8801
Borna Bešić <git.o8yim@aleeas.com>
2023-07-24 23:36:03 +0200

    Set MIN_LUMINANCE to 0.5

--- jagoda/Scenes/Game.gd
+++ jagoda/Scenes/Game.gd
@@ -3,7 +3,7 @@ extends Node2D
 const MAIN_MENU_SCENE = "res://Scenes/MainMenu.tscn"
 const DAY_LENGTH_MINUTES: int = 7
 const DAY_LENGTH_SECONDS: int = DAY_LENGTH_MINUTES * 60
-const MIN_LUMINANCE: float = 0.3
+const MIN_LUMINANCE: float = 0.5
 const MAX_LUMINANCE: float = 1.0
 const LUMINANCE_RANGE: float = MAX_LUMINANCE - MIN_LUMINANCE
 const MIN_SATURATION: float = 1.0
